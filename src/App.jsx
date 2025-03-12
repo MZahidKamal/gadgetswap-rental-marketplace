1614bc4709039ac094f0ba4c6e72f89da4e7feb0
MZahidKamal <kamal.md.zahid@gmail.com>
2025-03-12 18:16:03 +0100

    Project setup completed using Vite React ReactRouter and TailwindCSS.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+    return (
+        <div className={'border-2 border-red-500 font-bold text-5xl'}>
+            <h1>Hello World...</h1>
+        </div>
+    )
+}
+
+export default App
