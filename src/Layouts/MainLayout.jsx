50a2c906172bb500e3e7d3dec2b118073982b041
hossainshofiq <ah731710@gmail.com>
2025-03-15 00:48:57 +0600

    update signIn and signUp page

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import NavbarComponent from "../Components/NavbarComponent/NavbarComponent.jsx";
 import FooterComponent from "../Components/FooterComponent/FooterComponent.jsx";
-import {Outlet} from "react-router";
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
     return (
-        <div className={'container mx-auto'}>
-           
+        <div>
             <NavbarComponent></NavbarComponent>
-          
             <Outlet></Outlet>
             <FooterComponent></FooterComponent>
         </div>
